793d1ade8dbac3e1bf09ec3248e1bb6d4142b31e
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-08-22 22:30:18 +0530

    making schema for Products

---
+++ server/models/produce.model.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const Product = new mongoose.Schema({
+    uid: {type: String, required: true, unique: true},
+    produce: {type: Number, required: true},
+    dist: {type: String, required: true},
+    state: {type: String, required: true},
+});
+
+const model = mongoose.model('Product', Product);
+
+module.exports = model;
